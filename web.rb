355f635deb8c549feb9091d1a06050a62ee9302d
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-08-02 17:19:40 +0900

    add google analytics

--- web.rb
+++ web.rb
@@ -37,6 +37,17 @@ __END__
     %link(rel='stylesheet' href='/stylesheets/font-awesome.css')
     %link(rel='stylesheet' href='/stylesheets/foundation.css')
     %link(rel='stylesheet' href='/stylesheets/app.css')
+    :javascript
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', "#{ENV['UA_CODE']}"]);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
 
   %body
     %a(href='http://github.com/Tomohiro/fluentular')
